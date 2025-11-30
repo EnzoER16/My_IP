ede67b82ba17c47f44c3c3ddcbcfe6a2c43981a5
Enzo Rojas <enzo.e.rojas@gmail.com>
2025-11-30 17:24:59 -0300

    window properties updated

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main(page: ft.Page):
     page.window.width = 425
     page.window.height = 275
     page.window.resizable = False
+    page.window.maximizable = False
+    page.window.left = 560 # center x position for a 1920 value
+    page.window.top = 240 # center y position for a 1080 value
     page.add(ft.Text(""))
 
 ft.app(target=main)
