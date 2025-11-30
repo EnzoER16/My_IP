c722d13b7ac1442f95885a5a374927ee1df3bf96
Enzo Rojas <enzo.e.rojas@gmail.com>
2025-11-30 17:09:16 -0300

    window size added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import flet as ft
 
 def main(page: ft.Page):
-    pass
+    page.title = "My IP"
+    page.window.width = 425
+    page.window.height = 275
+    page.window.resizable = False
+    page.add(ft.Text(""))
 
 ft.app(target=main)
