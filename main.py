18120aaf793a98abcebe81352610b012352dc2f1
Enzo Rojas <enzo.e.rojas@gmail.com>
2025-12-01 21:27:29 -0300

    comments added to main.py

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ def main(page: ft.Page):
     page.window.left = 560 # center x position for a 1920 value
     page.window.top = 240 # center y position for a 1080 value
 
+    # window icon
     page.window.icon = "assets/icon.ico"
 
+    # window alignment
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
+    # add text to page
     page.add(ft.Text(f"IP: {get_ip()}", size=30, weight=ft.FontWeight.BOLD))
 
 ft.app(target=main)
